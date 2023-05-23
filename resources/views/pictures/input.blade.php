b0dbf19014087aa1f4c196fe721c22dd2ba4383b
rogelio1502 <rogelio15021515@gmail.com>
2023-05-23 16:27:20 -0600

    update Picture input blade

--- resources/views/pictures/input.blade.php
+++ resources/views/pictures/input.blade.php
@@ -1,9 +1,9 @@
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-<div class="rounded bg-white mb-1 p-4 mt-2">
-    <div data-controller="picture-input">
-        <div id="{{$id}}">
-            <picture-input title="{{$title}}" name="{{$name}}[]" isRequired="{{$required}}"></picture-input>
-        </div>
+
+<div data-controller="picture-input">
+    <div id="{{$id}}">
+        <picture-input title="{{$title}}" name="{{$name}}[]" isRequired="{{$required}}"></picture-input>
     </div>
 </div>
+
 <br>
