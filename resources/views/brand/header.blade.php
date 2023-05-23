3fddf000bf459753617f0ac7f78a5edb179ddd01
rogelio1502 <rogelio15021515@gmail.com>
2023-05-23 15:27:23 -0600

    rm display none class in some tags

--- resources/views/brand/header.blade.php
+++ resources/views/brand/header.blade.php
@@ -8,9 +8,9 @@
 
 
 <div class="h4 fw-light d-flex align-items-center justify-content-center">
-    <x-orchid-icon  path="orchid" width="1.3em" height="1.3em"/>
+    <x-orchid-icon path="orchid" width="1.3em" height="1.3em"/>
 
-    <p class="my-0 must-be-hide" style="margin-left: 10px">
+    <p class="my-0" style="margin-left: 10px">
         <span class="d-sm-block">
             {{env('APP_NAME')}}
         </span>
