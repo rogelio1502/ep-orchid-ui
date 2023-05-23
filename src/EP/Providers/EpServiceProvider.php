868e31d0ac50a4fe44675e9aa49105b459b1218b
rogelio1502 <rogelio15021515@gmail.com>
2023-05-23 15:30:24 -0600

    fix in service provider

--- src/EP/Providers/EpServiceProvider.php
+++ src/EP/Providers/EpServiceProvider.php
@@ -10,8 +10,8 @@ class EpServiceProvider extends ServiceProvider
 
     public function boot(Dashboard $ds)
     {
-        $ds->registerResource('scripts','/ep/js/ep-orchid-package.js');
-        $ds->registerResource('stylesheets','/ep/css/ep-orchid-package.css');
+        $ds->registerResource('scripts','/ep/js/ep-orchid-ui.js');
+        $ds->registerResource('stylesheets','/ep/css/ep-orchid-ui.css');
         $this->loadViewsFrom(__DIR__ . '/../../../resources/views', 'ep');
         $this->publishes([
             __DIR__.'/../../../resources/views/vendor' => resource_path('views/vendor'),
